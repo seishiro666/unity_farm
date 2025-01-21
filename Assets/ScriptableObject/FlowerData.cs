7ef9392f1f54a2fe27bcc94ad6ab97a94795eaa3
sei6hiro <153007490+sei6hiro@users.noreply.github.com>
2025-01-21 22:07:03 +0300

    add scriptableobject

--- Assets/ScriptableObject/FlowerData.cs
+++ Assets/ScriptableObject/FlowerData.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI; 
 [CreateAssetMenu(fileName = "newflower", menuName ="ScriptableObject/flower")]
 public class FlowerData : ScriptableObject
 {
-    
+    public string flowerName;
+    public int flowerPrice;
+    public float flowerRiseTime;
+    public float seedRiseTime;
+    public Sprite flowerIcon;
+    public Sprite seedIcon;
+    public int awardForFlower;
+    public GameObject flowerModel;
 }
